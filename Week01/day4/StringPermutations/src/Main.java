6d830fe0bf90c93519c6eab0697e25e6bd7ea40d
adambota <adambota@gmail.com>
2018-08-06 08:48:17 +0200

    Fixed program

--- Week01/day4/StringPermutations/src/Main.java
+++ Week01/day4/StringPermutations/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -8,7 +7,7 @@ public class Main {
             System.out.println(string + perm);
         } else {
             for (int i = 0; i < string.length(); i++) {
-                permutate(perm + string.charAt(i), string.substring(0, i));
+                permutate(perm + string.charAt(i), string.substring(0, i) + string.substring(i + 1, string.length()));
             }
         }
     }
@@ -19,7 +18,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int rnd = ThreadLocalRandom.current().nextInt(1, 100 + 1);
         String string = sc.nextLine();
         permutation(string);
     }
