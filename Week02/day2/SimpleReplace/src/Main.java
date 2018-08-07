b44d432a00166adaec483f6ba1037c33edc05f71
adambota <adambota@gmail.com>
2018-08-07 11:57:09 +0200

    feat(SimpleReplace): Finished exercise

---
+++ Week02/day2/SimpleReplace/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String example = "In a dishwasher far far away";
+        example = example.replace("dishwasher", "galaxy");
+        System.out.println(example);
+    }
+}
