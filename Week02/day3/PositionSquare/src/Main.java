3b6ebf208be9873796f8b5f46184c6d7d9317f10
adambota <adambota@gmail.com>
2018-08-08 12:55:17 +0200

    feat(PositionSquare): Finished exercise

---
+++ Week02/day3/PositionSquare/src/Main.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Main {
+
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+        drawSquare(graphics, 10, 10);
+        drawSquare(graphics, 170, 170);
+        drawSquare(graphics, 90, 90);
+    }
+
+    public static void drawSquare(Graphics graphics, int x, int y) {
+        graphics.drawRect(x, y, 50, 50);
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
