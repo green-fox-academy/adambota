bbd183eb031fcbf13226565902ce97a5b7850e59
adambota <adambota@gmail.com>
2018-08-08 12:04:37 +0200

    feat(HorizontalLines): Finished exercise

---
+++ Week02/day3/HorizontalLines/src/Main.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+import java.util.*;
+
+public class Main {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a 50 long horizontal line from that point.
+        // Draw 3 lines with that function. Use loop for that.
+        for (int i = 0; i < 3; i++) {
+            drawHorizontal(graphics, (int) (0 + Math.random() * (WIDTH - 50)), (int) (0 + Math.random() * HEIGHT));
+        }
+
+
+    }
+
+    public static void drawHorizontal(Graphics graphics, int x, int y) {
+        graphics.drawLine(x, y, x + 50 / 2, y);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
