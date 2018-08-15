547c95bc26e5ca067851da542f95c1913e7349d5
adambota <adambota@gmail.com>
2018-08-15 13:29:55 +0200

    feat(Animals): Finished exercise

---
+++ Week03/day3/Animal/src/Animal.java
@@ -0,0 +1,22 @@
+public class Animal {
+    int hunger;
+    int thirst;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger--;
+    }
+
+    public void drink() {
+        this.thirst--;
+    }
+
+    public void play() {
+        this.hunger++;
+        this.thirst++;
+    }
+}
