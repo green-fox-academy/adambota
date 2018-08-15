2e43eedde7381deb365fe116c589d7bd70020735
adambota <adambota@gmail.com>
2018-08-15 12:47:43 +0200

    feat(PostIt): Finished exercise

---
+++ Week03/day3/PostIt/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PostIt first = new PostIt("Orange", "Idea 1", "Blue");
+        PostIt second = new PostIt("Pink", "Awesome", "Black");
+        PostIt third = new PostIt("yellow", "Superb!", "Green");
+    }
+}
---
+++ Week03/day3/PostIt/src/PostIt.java
@@ -0,0 +1,11 @@
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
