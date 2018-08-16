b1c3d12a1335cb72717bc085306ffaa29ba71dfd
adambota <adambota@gmail.com>
2018-08-16 13:22:57 +0200

    feat(Fibonacci): Finished exercise

---
+++ Week03/day4/Fibonacci/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(8));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 1) return 0;
+        else if (n == 2) return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
