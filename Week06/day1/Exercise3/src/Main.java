ac5d1e459fc2c3727bfe95f29a86747cfc3031a8
adambota <adambota@gmail.com>
2018-09-10 13:34:38 +0200

    feat(Exercise3): Finished

---
+++ Week06/day1/Exercise3/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+        numbers.stream()
+                .filter(number -> number > 0)
+                .forEach(number -> System.out.println(number * number));
+    }
+}
