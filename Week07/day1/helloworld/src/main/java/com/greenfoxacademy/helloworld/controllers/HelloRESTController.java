66b63bd63a240ef581bf1da951976c4e582f0410
adambota <adambota@gmail.com>
2018-09-17 12:44:09 +0200

    Finished Hello User REST App

--- Week07/day1/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
+++ Week07/day1/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
@@ -2,13 +2,14 @@ package com.greenfoxacademy.helloworld.controllers;
 
 import com.greenfoxacademy.helloworld.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping("/greeting")
-    public Greeting greet() {
-        return new Greeting(1,"Hello World!");
+    public Greeting greet(@RequestParam("name") String name) {
+        return new Greeting(1,"Hello " + name);
     }
 }
