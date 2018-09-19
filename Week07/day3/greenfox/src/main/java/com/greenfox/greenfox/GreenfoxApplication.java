4ce2cbe3b68b9a42a3dc0ddbb68dcffb0d0cd59a
adambota <adambota@gmail.com>
2018-09-19 16:37:22 +0200

    feat(Greenfox): Finished adding new students

---
+++ Week07/day3/greenfox/src/main/java/com/greenfox/greenfox/GreenfoxApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenfoxApplication.class, args);
+    }
+}
---
+++ Week07/day3/greenfox/src/main/java/com/greenfox/greenfox/controllers/GreenfoxController.java
@@ -0,0 +1,41 @@
+package com.greenfox.greenfox.controllers;
+
+import com.greenfox.greenfox.services.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreenfoxController {
+
+    private StudentService studentService;
+
+    public GreenfoxController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @RequestMapping("/gfa")
+    public String gfa(Model model) {
+        model.addAttribute("list", "/gfa/list");
+        model.addAttribute("add", "/gfa/add");
+        return "gfa";
+    }
+
+    @RequestMapping("/gfa/list")
+    public String gfaList(Model model) {
+        model.addAttribute("list", studentService.findAll());
+        return "gfalist";
+    }
+
+    @RequestMapping("/gfa/add")
+    public String gfaAdd() {
+        return "gfaadd";
+    }
+
+    @RequestMapping("gfa/save")
+    public String gfaSave(@RequestParam("name") String name) {
+        studentService.save(name);
+        return "gfasave";
+    }
+}
