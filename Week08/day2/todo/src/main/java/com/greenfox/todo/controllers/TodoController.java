b867ac24a884bbfe62c14cc94f6453b8c7441cc4
adambota <adambota@gmail.com>
2018-09-25 14:48:37 +0200

    feat(Todo): Finished extending listing

--- Week08/day2/todo/src/main/java/com/greenfox/todo/controllers/TodoController.java
+++ Week08/day2/todo/src/main/java/com/greenfox/todo/controllers/TodoController.java
@@ -1,9 +1,12 @@
 package com.greenfox.todo.controllers;
 
+import com.greenfox.todo.models.Todo;
 import com.greenfox.todo.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class TodoController {
@@ -14,8 +17,12 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
-    @GetMapping("/todo")
-    public String list(Model model) {
+    @GetMapping(value = {"/", "/todo"})
+    public String list(Model model, String isActive) {
+        boolean listActives = false;
+        if (isActive.equals("true")) listActives = true;
+
+        model.addAttribute("listActives", listActives);
         model.addAttribute("todos", todoRepository.findAll());
         return "todolist";
     }
