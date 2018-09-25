3f43255befd12c13adc91c11edb62e465769ae8c
adambota <adambota@gmail.com>
2018-09-25 15:24:29 +0200

    feat(Todo): Added creating function

--- Week08/day2/todo/src/main/java/com/greenfox/todo/controllers/TodoController.java
+++ Week08/day2/todo/src/main/java/com/greenfox/todo/controllers/TodoController.java
@@ -5,8 +5,12 @@ import com.greenfox.todo.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TodoController {
@@ -18,12 +22,23 @@ public class TodoController {
     }
 
     @GetMapping(value = {"/", "/todo"})
-    public String list(Model model, String isActive) {
+    public String list(Model model, @RequestParam(value = "isActive", required = false) String isActive) {
         boolean listActives = false;
-        if (isActive.equals("true")) listActives = true;
+        if (isActive != null && isActive.equals("true")) listActives = true;
 
         model.addAttribute("listActives", listActives);
         model.addAttribute("todos", todoRepository.findAll());
         return "todolist";
+    }
+
+    @GetMapping("/todo/add")
+    public String addGet() {
+        return "todoadd";
+    }
+
+    @PostMapping("/todo/add")
+    public String addPost(String title) {
+        todoRepository.save(new Todo(title));
+        return "redirect:/todo/?isActive=false";
     }
 }
